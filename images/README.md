1be2aa4d11f558de40bae78290b67f4eeeacb55c
Zsobix <index@zsobix.xyz>
2026-07-19 22:52:37 +0200

    feat: finalize favorites, add weather, and stardance stats

--- images/README.md
+++
@@ -1,9 +0,0 @@
-# Images
-
-Images in this folder that are marked with "CC" in the filename are licensed under [CC BY-NC-SA][cc-by-nc-sa].
-
-[![CC BY-NC-SA 4.0][cc-by-nc-sa-image]][cc-by-nc-sa]
-
-[cc-by-nc-sa]: http://creativecommons.org/licenses/by-nc-sa/4.0/
-[cc-by-nc-sa-image]: https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png
-[cc-by-nc-sa-shield]: https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg
--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "
 const favorite = document.getElementById("addfavorite")
 const buttons = document.getElementsByClassName("favorite")
 const re2 = /(^https?:\/\/(?:.*\.)?.*\.[a-zA-Z]*).*/;
+const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+const trashcans = document.getElementsByClassName("trash")
+let latitude;
+let longitude;
+
 
 var date = new Date();
 clock.innerHTML = String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0')
@@ -19,42 +24,99 @@ if (localStorage.getItem("favorites") == "" || localStorage.getItem("favorites")
     var favorites = JSON.parse(localStorage.getItem("favorites"));
 }
 
+if (localStorage.getItem("username") != null) {
+    var username = localStorage.getItem("username");
+    console.log(username)
+    console.log(username != null)
+    fetch(`https://stardance.jam06452.uk/api/v2/users/${username}/projects?limit=100`)
+        .then(response => {
+            if (response.ok) {
+                response.json().then((jsonResponse) => {
+                    console.log(jsonResponse)
+                    var obj = jsonResponse;
+                    var maxhours = 0;
+                    var maxdevlogs = 0;
+                    var maxfollowers = 0;
+                    for (let i = 0; i < obj.projects.length; i++) {
+                        var hours = parseInt(obj.projects[i].total_hours)
+                        maxhours = hours+maxhours
+                        var devlogs = parseInt(obj.projects[i].devlog_count)
+                        maxdevlogs = devlogs+maxdevlogs
+                        var followers = parseInt(obj.projects[i].followers)
+                        maxfollowers = followers+maxfollowers
+                    }
+                    document.getElementById("stardance").innerHTML = `<i class="fa-solid fa-star"></i> Stardance Stats:<br>
+                    <i class="fa-solid fa-book"></i> Total devlogs: ${maxdevlogs}<br>
+                    <i class="fa-solid fa-user-plus"></i> Total followers (across all projects): ${maxfollowers}<br>
+                    <i class="fa-regular fa-clock"></i> Total hours: ${maxhours} h`
+                    document.getElementById("error01").style.display = "none"
+                    document.getElementById("error02").style.display = "none"
+                    localStorage.setItem("username", username)
+            })
+            } else {
+                document.getElementById("error01").style.display = "block"
+            }
+        })
+}
+
 for (let i = 0; i < favorites.length; i++) {
     let index;
     var fav = favorites[i];
-    if (i == 1) {
+    let del_index;
+    del_index = i 
+    if (i == 2) {
+        index = 3
+    } else if (i == 3) {
+        index = 4
+    } else if (i == 4) {
         index = 2
-    } else if (i == 2) {
-        index = 1
+        del_index = 2
     } else {
-        index = 0
+        index = i
     }
-    var shorten = fav[0].substring(0,6)
-    if (shorten.length == 6){
+    var shorten = fav[0].substring(0,7)
+    if (fav[0].length > shorten.length){
         shorten = `${shorten}...`
     }
-    buttons[index].innerHTML = `<div class="favbox">${fav[0].charAt(0)}</div><p class="favtext">${shorten}</p>`
+    var img = `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${fav[1]}&size=48`
+    buttons[index].innerHTML = `<a href="${fav[2]}"><div class="favbox"><img src="${img}"></div></a><p class="favtext">${shorten}</p><div class="trash"><i class="fa-solid fa-trash"></i></div>`
     buttons[index].style.opacity = "100%"
-    buttons[index].onclick = function(){
-        window.location.href = fav[2];
-    };
-}
-
-
-const reload = function refreshCSS() {
-    let links = document.getElementsByTagName('link');
-    for (let i = 0; i < links.length; i++) {
-        if (links[i].getAttribute('rel') == 'stylesheet') {
-            let href = links[i].getAttribute('href')
-                                    .split('?')[0];
-            
-            let newHref = href + '?version=' 
-                        + new Date().getMilliseconds();
-            
-            links[i].setAttribute('href', newHref);
-        }
+    trashcans[del_index].onclick = function() {
+        favorites.splice(i, 1)
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+        window.location.reload()
     }
 }
+const getDirection = function getDirection(angle) {
+    // Source - https://stackoverflow.com/a/48750814
+    // Posted by VisioN, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-07-19, License - CC BY-SA 4.0 
+
+    var index = Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8;
+    return directions[index]
+}
+function success(pos) {
+    const crd = pos.coords;
+
+    console.log(`Latitude: ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${crd.latitude}&longitude=${crd.longitude}&daily=temperature_2m_max,temperature_2m_min&current=temperature_2m,is_day,apparent_temperature,precipitation,wind_speed_10m,wind_direction_10m&forecast_days=1`)
+        .then(response => {response.json().then((jsonResponse) => {
+            console.log(jsonResponse)
+            var obj = jsonResponse;
+            document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-sun"></i> ${obj.current.temperature_2m}°C (feels like ${obj.current.apparent_temperature}°C), <br>
+            max ${obj.daily.temperature_2m_max[0]}°C, min ${obj.daily.temperature_2m_min[0]}°C<br>
+            <i class="fa-solid fa-wind"></i> ${obj.current.wind_speed_10m} km/h from ${getDirection(obj.current.wind_direction_10m)}<br>
+            <i class="fa-solid fa-cloud-rain"></i> ${obj.current.precipitation} mm (approximately)`
+        })})
+}
+
+function error(err) {
+    document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-sun"></i> ERROR:<br> Location usage denied,<br> or other error... <br>`
+}
+
+window.navigator.geolocation.getCurrentPosition(success, error)
+
 
 
 const keyHandler = function keyHandler(e) {
@@ -99,33 +161,46 @@ document.getElementById("save").onclick = function() {
     let temp_fav;
     var name = document.getElementById("name") 
     var url = document.getElementById("url")
-    if (url.value == "" || name.value == "") {
+    if (url.value == "") {
         document.getElementById("error2").style.display = "block"
     } else if (url.checkValidity()) {
         var regMatch = url.value.match(re2)
         // name, url (just domain), url (whole url)
+        if (name.value == "") {
+            name.value = regMatch[1].replace(/^https?:\/\//, "")
+        }
         temp_fav = [name.value, regMatch[1], regMatch[0]]
         favorites.push(temp_fav)
         for (let i = 0; i < favorites.length; i++) {
             let index;
             var fav = favorites[i];
-            if (i == 1) {
+            let del_index;
+            del_index = i 
+            if (i == 2) {
+                index = 3
+            } else if (i == 3) {
+                index = 4
+            } else if (i == 4) {
                 index = 2
-            } else if (i == 2) {
-                index = 1
+                del_index = 2
             } else {
-                index = 0
+                index = i
             }
-            var shorten = fav[0].substring(0,6)
-            if (shorten.length == 6){
+
+            var shorten = fav[0].substring(0,7)
+            if (fav[0].length > shorten.length){
                 shorten = `${shorten}...`
             }
-            buttons[index].innerHTML = `<div class="favbox">${fav[0].charAt(0)}</div><p class="favtext">${shorten}</p>`
+            var img = `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${fav[1]}&size=48`
+            buttons[index].innerHTML = `<a href="${fav[2]}"><div class="favbox"><img src="${img}"></div></a><p class="favtext">${shorten}</p><div class="trash"><i class="fa-solid fa-trash"></i></div>`
             buttons[index].style.opacity = "100%"
-            buttons[index].onclick = function(){
-                window.location.href = fav[2];
-            };
+            trashcans[del_index].onclick = function() {
+                favorites.splice(i, 1)
+                localStorage.setItem("favorites", JSON.stringify(favorites));
+                window.location.reload()
+            }
         }
+
         name.value = ""
         url.value = ""
         document.getElementById("savepage").style.display = "none"
@@ -133,6 +208,46 @@ document.getElementById("save").onclick = function() {
         localStorage.setItem("favorites", JSON.stringify(favorites));
     } else {
         document.getElementById("error1").style.display = "block"
+    }
+}
+
+document.getElementById("starsave").onclick = function(){
+    var username = document.getElementById("starinput").value;
+    if (username == "") {
+        document.getElementById("error02").style.display = "block"
+    } else {
+        fetch(`https://stardance.jam06452.uk/api/v2/users/${username}/projects?limit=100`)
+            .then(response => {
+                if (response.ok) {
+                    response.json().then((jsonResponse) => {
+                        console.log(jsonResponse)
+                        var obj = jsonResponse;
+                        var maxhours = 0;
+                        var maxdevlogs = 0;
+                        var maxfollowers = 0;
+                        for (let i = 0; i < obj.projects.length; i++) {
+                            var hours = parseInt(obj.projects[i].total_hours)
+                            maxhours = hours+maxhours
+
+                            var devlogs = parseInt(obj.projects[i].devlog_count)
+                            maxdevlogs = devlogs+maxdevlogs
+
+                            var followers = parseInt(obj.projects[i].followers)
+                            maxfollowers = followers+maxfollowers
+                        }
+                        document.getElementById("stardance").innerHTML = `<i class="fa-solid fa-star"></i> Stardance Stats:<br>
+                        <i class="fa-solid fa-book"></i> Total devlogs: ${maxdevlogs}<br>
+                        <i class="fa-solid fa-user-plus"></i> Total followers (across all projects): ${maxfollowers}<br>
+                        <i class="fa-regular fa-clock"></i> Total hours: ${maxhours} h`
+                        document.getElementById("error01").style.display = "none"
+                        document.getElementById("error02").style.display = "none"
+                })
+                localStorage.setItem("username", username)
+                document.getElementById("starinput").value = ""
+                } else {
+                    document.getElementById("error01").style.display = "block"
+                }
+            })
     }
 }
 
